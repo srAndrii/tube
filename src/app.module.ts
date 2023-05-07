a0f04fae28b47d783382bd4ec21160e69ab69bea
srAndrii <55akif5548@gmail.com>
2023-05-07 14:08:35 -0700

    Linked to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getTypeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getTypeOrmConfig
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/config/jwt.config.ts
@@ -0,0 +1,8 @@
+import { ConfigService } from "@nestjs/config";
+import { JwtModuleOptions } from "@nestjs/jwt";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const getJwtConfig = async (configService: ConfigService): Promise<JwtModuleOptions> => ({
+    secret: configService.get("JWT_SECRET"),
+
+})
