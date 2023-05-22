0b9127d14a4d75a245709a38aa2a5996c3041574
srAndrii <55akif5548@gmail.com>
2023-05-22 14:32:36 -0700

    VideoEntity

---
+++ src/video/video.entity.ts
@@ -0,0 +1,44 @@
+import { CommentEntity } from "src/comment/comment.entity";
+import { UserEntity } from "src/user/user.entity";
+import { Base } from "src/utils/base";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from "typeorm";
+
+@Entity('Video')
+export class VideoEntity extends Base {
+    // @ManyToOne(() => UserEntity, user => user.videos)
+    // @JoinColumn({ name: 'user_id' })
+    // user: UserEntity
+
+
+
+
+    @Column({ default: '' })
+    name: string
+
+    @Column({ default: 0 })
+    views?: number
+
+    @Column({ default: 0 })
+    likes?: number
+
+    @Column({ default: 0 })
+    duration?: number
+
+    @Column({ default: '', type: 'text' })
+    description: string
+
+    @Column({ default: '', name: 'video_path' })
+    videoPath: string
+
+    @Column({ default: '', name: 'thumbnail_path' })
+    thumbnailPath: string
+
+    @ManyToOne(() => UserEntity, user => user.videos)
+    @JoinColumn({ name: 'user_id' })
+    user: UserEntity
+
+    @OneToMany(() => CommentEntity, comment => comment.video)
+    comments: CommentEntity[]
+
+
+}
