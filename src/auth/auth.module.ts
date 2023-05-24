0d011f2b026bd89cad70ee7cd6313f7223442092
srAndrii <55akif5548@gmail.com>
2023-05-23 19:53:43 -0700

    Work with Auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { getJwtConfig } from 'src/config/jwt.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/user.entity';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig
+    }),
+    TypeOrmModule.forFeature([UserEntity])
+  ]
+
 })
-export class AuthModule {}
+export class AuthModule { }
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common"
+import { ConfigService } from "@nestjs/config"
+import { PassportStrategy } from "@nestjs/passport"
+import { InjectRepository } from "@nestjs/typeorm"
+import { UserEntity } from "src/user/user.entity"
+import { Repository } from "typeorm"
+import { ExtractJwt, Strategy } from "passport-jwt"
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private readonly configService: ConfigService,
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: true,
+            secretOrKey: configService.get('JWT_SECRET'),
+        })
+    }
+
+    async validate({ id }: Pick<UserEntity, 'id'>) {
+        return this.userRepository.findBy({ id })
+    }
+}
