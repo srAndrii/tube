72ce53b124def0705935d4fc93593eacdcf6bc6a
srAndrii <55akif5548@gmail.com>
2023-06-17 17:06:57 -0700

    Work with Comment Controller

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
-import { CommentService } from './comment.service';
+import {
+	Body,
+	Controller,
+	HttpCode,
+	Post,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { CommentService } from './comment.service'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CurrentUser } from 'src/user/user.decorator'
+import { CommentDto } from './comment.dto'
 
 @Controller('comment')
 export class CommentController {
-  constructor(private readonly commentService: CommentService) {}
+	constructor(private readonly commentService: CommentService) {}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post()
+	@Auth()
+	async createComment(@CurrentUser('id') id: string, @Body() dto: CommentDto) {
+		return this.commentService.create(Number(id), dto)
+	}
 }
